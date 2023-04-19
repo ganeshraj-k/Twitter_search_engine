9ee97319c0d827fdeddbd53b1df73f05159b7add
Noopur Singh <76904642+Noopur665@users.noreply.github.com>
2023-04-18 21:44:29 -0400

    Changes made with respect to fetch retweets and get tweets for user
    Changes made with respect to fetch retweets and get tweets for user in API and html page.

--- test.py
+++ test.py
@@ -5,23 +5,35 @@ app=Flask(__name__)
 
 @app.route('/search',methods=['POST'])
 def search():
+    userdata=""
 	search_str = request.form['searchstring']
 	if search_str.startswith('@'):
 		search_str = search_str[1:]
-		data = get_info_by_user(search_str)
+		userdata = get_info_by_user(search_str)
 	elif search_str.startswith('#'):
 		data = get_info_by_hashtag(search_str[1:])
 	else:
 		data = get_info_by_tweet(tweet_str = search_str)
 	print(data)
-	return render_template('mypage.html',data=data,userdata="")
+	return render_template('mypage.html',data=data,userdata=userdata)
 
 @app.route('/user',methods=['GET'])
 def getuser():
     uname = request.args.get('user')
     userdata = get_info_by_user(uname)
-    print(userdata)
     return render_template('mypage.html',userdata=userdata,data="")
+
+@app.route('/userTweet', methods=["GET"])
+def getTweetsByUser():
+    uname = request.args.get('userid')
+    tweetdata = get_info_by_user(uname)
+    return render_template('mypage.html',userdata="",data=tweetdata)
+
+@app.route('/retweets', methods=["GET"])
+def getretweetsbyTweetID():
+    octweetid = request.args.get('tweetid')
+    tweetdata = get_info_by_tweet(oc_tweet_id=octweetid)
+    return render_template('mypage.html',userdata="",data=tweetdata)
 
 @app.route('/')
 def load():
