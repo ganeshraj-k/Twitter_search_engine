bf85f3fa169a643187213a822086fc20a7ec1ded
Noopur Singh <76904642+Noopur665@users.noreply.github.com>
2023-04-20 20:46:37 -0400

    Changes in cache
    Changes in cache

--- python_cache_demo.py
+++ python_cache_demo.py
@@ -26,12 +26,12 @@ def Search_Cache(search_string):
     if(bool(cached_data)):
         cached_queries, potential_cached_queries = get_top_cache_data()
         if(bool(cached_queries)):
-            print('hewafas')
+            print('hewafas 2')
             print(search_string)
             match_query = process.extractOne(search_string, cached_queries , scorer = fuzz.token_set_ratio)
             print(match_query)
-            if(((search_string == 'top_10_tweets') | (search_string == 'top_10_users')) & (match_query[1]>60)):
-                print('hrsbgfdnhfujtfy')
+            if(((search_string == 'top_10_tweets' and match_query[1]>74) | (search_string == 'top_10_users' and match_query[1]>64))):
+                
                 cached_data[search_string]['counter']+=1
                 search_result[0] = cached_data[search_string]['result']
                 print('vueobvuofsbdvpud')
@@ -40,33 +40,40 @@ def Search_Cache(search_string):
                 update_cache()
                 search_result[1]=1
                 return search_result
-            elif(match_query[1]>50):
-                cached_data[match_query[0]]['counter']+=1
-                search_result[0] = cached_data[match_query[0]]['result']
-                #Calling refresh
-                cached_data = refresh()
-                update_cache()
-                search_result[1]=1
-                return search_result
-            elif(potential_cached_queries):
-                match_query = process.extractOne(search_string, potential_cached_queries)
-                if(match_query[1]>50): ## potential error
+            elif(search_string != 'top_10_tweets' and search_string != 'top_10_users'):
+                print('hrsbgfdnhfujtfy')
+                if(match_query[1]>50):
                     cached_data[match_query[0]]['counter']+=1
+                    search_result[0] = cached_data[match_query[0]]['result']
                     #Calling refresh
                     cached_data = refresh()
                     update_cache()
                     search_result[1]=1
                     return search_result
+                elif(potential_cached_queries):
+                    match_query = process.extractOne(search_string, potential_cached_queries)
+                    if(match_query[1]>50): ## potential error
+                        cached_data[match_query[0]]['counter']+=1
+                        #Calling refresh
+                        cached_data = refresh()
+                        update_cache()
+                        search_result[1]=1
+                        return search_result
+    print("The search Result : ",search_result)
     return search_result
 
 def Write_Cache(search_string,search_result):
     global cached_data
+    print("inside write 234")
+    print("hjsfgssjhf",cached_data)
     if(len(cached_data)<20):
         cached_data[search_string] = {
             "counter":1,
             "result":search_result
         }
+        print("inside len")
         cached_data = refresh()
+        print("inside write 1",cached_data)
         update_cache()
             
 # Refresh the dictionary with sorted count
@@ -86,6 +93,7 @@ def update_cache():
     global cached_data
     cached_data_write={"cached_queries":cached_data}
     json_string = json.dumps(cached_data_write)
+    print("inside update 2",json_string)
     with open("CacheFile.json","w") as cache_file:
         cache_file.write(json_string);
        
